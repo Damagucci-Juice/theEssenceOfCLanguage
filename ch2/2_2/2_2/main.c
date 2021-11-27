da2c9d5429e5db69b851a30538071ff0e5b4465a
Damagucci-Juice <jsr9874@gmail.com>
2021-11-27 17:31:15 +0900

    revert commit

---
+++ ch2/2_2/2_2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  2_2
+//
+//  Created by YEONGJIN JANG on 2021/11/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
