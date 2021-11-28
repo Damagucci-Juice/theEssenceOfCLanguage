bda49ce84ef0ed6d5db671dc666d09a78e2c3e90
Damagucci-Juice <jsr9874@gmail.com>
2021-11-28 12:34:34 +0900

    add scanf placticing file

---
+++ ch2/_8/_8/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int num;
+    
+    printf("Input an integer>");
+    scanf("%d", &num);
+    
+    printf("The number you entered is %d. \n", num);
+    
+    return 0;
+}
