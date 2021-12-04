d4ed8795c2ee3d30f3189c5a77c67ee425bfe40e
Damagucci-Juice <jsr9874@gmail.com>
2021-12-04 21:11:52 +0900

    add seldom study c lang.

---
+++ ch3/_13/_13/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    double pi = 3.141592;
+    double shortPi = (int)(pi * 1000 + 0.5) / 1000.0;
+    
+    printf("%5.3lf\n", shortPi);
+    
+    return 0;
+}
